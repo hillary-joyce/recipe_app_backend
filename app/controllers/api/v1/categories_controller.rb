8ad0b5410514fb003f676735d5b9a2aeaee36bad
github username <github email address>
2019-01-09 11:24:23 -0500

    finished categories controller

--- app/controllers/api/v1/categories_controller.rb
+++ app/controllers/api/v1/categories_controller.rb
@@ -1,19 +1,36 @@
 class Api::V1::CategoriesController < ApplicationController
+  before_action :find_category, only: [:update, :show, :destroy]
+
   def index
     @categories = Category.all
     render json: @categories
   end
 
   def show
+    render json: @category
   end
 
   def create
+    @category = Category.create(category_params)
+    render json: @category
   end
 
   def update
+    @category.update(category_params)
+    render json: @category
   end
 
-  def delete
+  def destroy
+    render json: Category.destroy(@category)
   end
 
+  private
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
+  def find_category
+    @category = Category.find(params[:id])
+  end
 end
